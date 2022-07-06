431a80aa3f64d40c72068c43966f939156ec0aa2
Panithan Bunrong <Panithan.bunrong@gmail.com>
2022-07-06 15:20:43 +0700

    Test commit

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
       <a-layout-content
         :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
       >
-        Content
+        test
       </a-layout-content>
     </a-layout>
   </a-layout>
